15dc60b45cccfaf842af914d18308d8dcc0868ab
Miguel Guedes <miguel@softgeist.com>
2024-06-21 10:18:46 +0100

    Prepare for publishing

---
+++ rollup.config.mjs
@@ -0,0 +1,33 @@
+import { readFile } from "node:fs/promises";
+
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+const pkgjson = JSON.parse(
+  await readFile(new URL(process.cwd() + "/package.json", import.meta.url)),
+);
+
+const canonicalName = pkgjson.name.split("/").slice(-1)[0];
+const bundleName = `dist/${canonicalName}`;
+
+export function buildConfig() {
+  return {
+    input: "src/index.ts",
+    output: [
+      {
+        file: `${bundleName}.cjs.js`,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: `${bundleName}.esm.js`,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [resolve(), commonjs(), typescript()],
+  };
+}
+
+export default buildConfig();
